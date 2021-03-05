7077c5056d42b6fc4d960514e6aedae139ce650d
Alan Mak <alanmak95@gmail.com>
2021-03-05 18:42:25 -0500

    Created red screen

--- src/components/landing/red/red.jsx
+++ src/components/landing/red/red.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import Button2 from '../button2'
+import Button1 from '../button1';
+import Button2 from '../button2';
+import Nav from '../../nav';
+
+import './red.scss'
+
 export default function Red(props) {
   return(
-    <>
+    <div id='red'>
+      <Nav/>
+      <Button1 />
       <Button2 />
-    </>
+    </div>
   )
 }
