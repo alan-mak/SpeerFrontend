5ee2d4256e353c0f51f2f0fecc45be844f4d930b
Alan Mak <alanmak95@gmail.com>
2021-03-04 20:29:48 -0500

    Created hamburger nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './nav.scss'
+import './nav.scss';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Nav () {
+  const [menu, setMenu] = useState(false);
 
   const navItem = [{
     path:"/perks",
@@ -21,8 +22,12 @@ export default function Nav () {
 
   return (
     <div className="burger">
-      <FontAwesomeIcon icon={faBars} /> EXP | CON
-      <ul>
+      <FontAwesomeIcon icon={faBars}  onClick={()=> {
+        menu ? (setMenu(false)): (
+          setMenu(true)
+          )
+      }}/> EXP | CON
+      <ul className={menu ? 'ul--show': 'ul--hide'}>
         {navItem.map(item => <li>
           <a href={item.path}>{item.content}</a>
           </li>)}
