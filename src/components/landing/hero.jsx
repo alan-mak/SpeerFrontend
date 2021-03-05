2db07471270632821c8e935aa9ce3ca522b01a1b
Alan Mak <alanmak95@gmail.com>
2021-03-05 11:16:03 -0500

    Added image carousel

--- src/components/landing/hero.jsx
+++ src/components/landing/hero.jsx
@@ -1,2 +1,30 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './hero.scss'
+import { Carousel } from 'react-responsive-carousel';
+
+import img1 from '../../img/taylor1.jpg';
+import img2 from '../../img/taylor2.jpg';
+import img3 from '../../img/taylor3.png';
+
+export default function Hero(props) {
+
+
+
+  return (
+    <Carousel infiniteLoop autoPlay>
+      <div>
+        <img src={img1} alt="Taylor 1"/>
+        <p className="legend">Taylor 1</p>
+      </div>
+      <div>
+        <img src={img2} alt="Taylor 2"/>
+        <p className="legend">Taylor 2</p>
+      </div>
+      <div>
+        <img src={img3} alt="Taylor 3"/>
+        <p className="legend">Taylor 3</p>
+      </div>
+    </Carousel>
+  )
+}
--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-import Button1 from './button1'
+import Button1 from './button1';
+import Hero from './hero';
 
 export default function Landing(props) {
 
   return(
     <>
       <Button1 />
+      <Hero />
     </>
   )
 }
