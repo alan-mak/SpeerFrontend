91ab92b5d36b4e7c28991318d87f6f4337b36d1f
Alan Mak <alanmak95@gmail.com>
2021-03-04 22:53:26 -0500

    Added Landing page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Nav from './components/nav';
+import Landing from './components/landing';
 
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Landing />
     </div>
   );
 }
---
+++ src/components/landing/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Nav from './nav'
+
+export default function Landing(props) {
+
+  return(
+    <Nav />
+  )
+}
