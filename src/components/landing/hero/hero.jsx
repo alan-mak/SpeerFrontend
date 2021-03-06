0cbd6801db705d25c6ad6e32325ff1a807111c21
Alan Mak <alanmak95@gmail.com>
2021-03-05 20:08:38 -0500

    Changed button to pill shape

--- src/components/landing/hero/hero.jsx
+++ src/components/landing/hero/hero.jsx
@@ -3,6 +3,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './hero.scss'
 import { Carousel } from 'react-responsive-carousel';
 
+import Button1 from '../button1'
+
 import img1 from '../../../img/taylor1.jpg';
 import img2 from '../../../img/taylor2.jpg';
 import img3 from '../../../img/taylor3.png';
@@ -12,16 +14,21 @@ export default function Hero(props) {
 
 
   return (
-    <Carousel infiniteLoop autoPlay showArrows={false} showThumbs={false} interval={10000} showStatus={false} dynamicHeight={true} stopOnHover={false}>
-      <div>
-        <img src={img1} alt="Taylor 1"/>
+    <div id="hero">
+      <div id="hero-liquid">
+        <Button1 />
       </div>
-      <div>
-        <img src={img2} alt="Taylor 2"/>
-      </div>
-      <div>
-        <img src={img3} alt="Taylor 3"/>
-      </div>
-    </Carousel>
+      <Carousel infiniteLoop autoPlay showArrows={false} showThumbs=  {false} interval={10000} showStatus={false} dynamicHeight={true}  stopOnHover={false}>
+        <div>
+          <img src={img1} alt="Taylor 1"/>
+        </div>
+        <div>
+          <img src={img2} alt="Taylor 2"/>
+        </div>
+        <div>
+          <img src={img3} alt="Taylor 3"/>
+        </div>
+      </Carousel>
+    </div>
   )
 }
--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Button1 from './button1';
 import Hero from './hero/hero';
 import Red from './red/red';
 
@@ -8,7 +7,6 @@ export default function Landing(props) {
 
   return(
     <>
-      <Button1 />
       <Hero />
       <Red />
     </>
